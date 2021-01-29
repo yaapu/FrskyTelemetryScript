601ed078ccaa80dcf19f62c59ce9150828f8504c
Risto <rotorman@users.noreply.github.com>
2021-01-29 17:27:41 +0100

    Update main.lua
    Removed misleading dilution of precision unit (it's unitless) Issue #88

--- HORUS/SOURCES/SRC/WIDGETS/Yaapu/main.lua
+++ HORUS/SOURCES/SRC/WIDGETS/Yaapu/main.lua
@@ -1242,7 +1242,7 @@ local function processTelemetry(DATA_ID,VALUE)
     -- offset  4: NO_GPS = 0, NO_FIX = 1, GPS_OK_FIX_2D = 2, GPS_OK_FIX_3D or GPS_OK_FIX_3D_DGPS or GPS_OK_FIX_3D_RTK_FLOAT or GPS_OK_FIX_3D_RTK_FIXED = 3
     -- offset 14: 0: no advanced fix, 1: GPS_OK_FIX_3D_DGPS, 2: GPS_OK_FIX_3D_RTK_FLOAT, 3: GPS_OK_FIX_3D_RTK_FIXED
     telemetry.gpsStatus = bit32.extract(VALUE,4,2) + bit32.extract(VALUE,14,2)
-    telemetry.gpsHdopC = bit32.extract(VALUE,7,7) * (10^bit32.extract(VALUE,6,1)) -- dm
+    telemetry.gpsHdopC = bit32.extract(VALUE,7,7) * (10^bit32.extract(VALUE,6,1))
     telemetry.gpsAlt = bit32.extract(VALUE,24,7) * (10^bit32.extract(VALUE,22,2)) * (bit32.extract(VALUE,31,1) == 1 and -1 or 1)-- dm
   elseif DATA_ID == 0x5003 then -- BATT
     telemetry.batt1volt = bit32.extract(VALUE,0,9)
